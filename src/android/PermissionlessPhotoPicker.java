be19bc73fc94eab5e6a1306b196dfe4e5604ea5f
jcesarmobile <jcesarmobile@gmail.com>
2024-07-28 01:43:33 +0200

    fix(android): call registerForActivityResult on pluginInitialize (#3)

--- src/android/PermissionlessPhotoPicker.java
+++ src/android/PermissionlessPhotoPicker.java
@@ -10,19 +10,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 public class PermissionlessPhotoPicker extends CordovaPlugin {
-
     private static final String PICK_IMAGES = "pickImages";
+    private CallbackContext callbackContext;
+    private ActivityResultLauncher<PickVisualMediaRequest> pickMedia;
+
+
+    @Override
+    protected void pluginInitialize() {
+        super.pluginInitialize();
+        pickMedia =
+                cordova.getActivity().registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
+                    if (uri != null) {
+                        callbackContext.success(String.valueOf(uri));
+                    } else {
+                        callbackContext.error("no image picked");
+                    }
+                });
+    }
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals(PICK_IMAGES)) {
-            ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
-                    cordova.getActivity().registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), uri -> {
-                        if (uri != null) {
-                            callbackContext.success(String.valueOf(uri));
-                        } else {
-                            callbackContext.error("no image picked");
-                        }
-                    });
+            this.callbackContext = callbackContext;
             pickMedia.launch(new PickVisualMediaRequest.Builder()
                     .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
                     .build());
